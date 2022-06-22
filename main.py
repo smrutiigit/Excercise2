4a79660248f9d96ce4ce1a767fc91dc16b82cd97
smrutiigit <106029838+smrutiigit@users.noreply.github.com>
2022-06-22 06:51:36 +0530

    adding post method for creating emplyoee details

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-from typing import Union
-
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    first_name: str
+    last_name:str
+    employee_id: str
+    city: str
+    experience: float
+    ctc:float
+    age:float
+    contact: str
 
 app = FastAPI()
-
-
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+@app.post("/items/")
+def create_item(item:Item):
+   return item
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
